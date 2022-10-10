c93f3b9dea5d9dcb4cc3bccb6c536ba90ec1f9d3
rutatiina <rutatiina@ruginem.org>
2022-10-10 11:39:52 +0300

    Add soft deleting to all models

--- src/Models/CreditNote.php
+++ src/Models/CreditNote.php
@@ -3,11 +3,13 @@
 namespace Rutatiina\CreditNote\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Activitylog\Traits\LogsActivity;
 use Rutatiina\Tenant\Scopes\TenantIdScope;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreditNote extends Model
 {
+    use SoftDeletes;
     use LogsActivity;
 
     protected static $logName = 'Txn';
@@ -49,7 +51,7 @@ class CreditNote extends Model
 
         static::addGlobalScope(new TenantIdScope);
 
-        self::deleting(function($txn) { // before delete() method call this
+        self::deleted(function($txn) { // before delete() method call this
              $txn->items()->each(function($row) {
                 $row->delete();
              });
@@ -58,6 +60,18 @@ class CreditNote extends Model
              });
              $txn->ledgers()->each(function($row) {
                 $row->delete();
+             });
+        });
+
+        self::restored(function($txn) {
+             $txn->items()->each(function($row) {
+                $row->restore();
+             });
+             $txn->comments()->each(function($row) {
+                $row->restore();
+             });
+             $txn->ledgers()->each(function($row) {
+                $row->restore();
              });
         });
 
